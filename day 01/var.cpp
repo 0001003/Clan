437fb549b73578f0fd9f9161bd6ffa6afbdd85a8
0001003 <158238077+0001003@users.noreply.github.com>
2024-01-31 12:48:38 +0900

    Add files via upload

---
+++ day 01/var.cpp
@@ -0,0 +1,23 @@
+//var
+#include<stdio.h>
+
+int main() {
+
+	// 30 + 20 = 50 사람 >> 연산 + 기억 동시에
+	// 30 + 20 = 50 컴퓨터 >> 연산, 기억 따로
+
+
+	// 정수or실수or... 변수
+	// int - integer[독일어로 정수라는 뜻]
+
+	int num = 100;
+	int son = 7;
+
+
+
+	printf("num의 값은: %d\n", num);
+	printf("num + son = %d", num + son);
+
+
+	return 0;
+}
